b7ef743e51d1f3e46eba85970619ba754bb19050
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-05 20:26:50 +0600

    Add files via upload

---
+++ habluExample/operators.py
@@ -0,0 +1,32 @@
+# addition
+a = 5
+b = 2
+print(a + b)
+
+# subtraction
+print(a - b)
+
+# multiplication
+print(a * b)
+
+# division
+print(a / b)
+
+# modulus
+print(a % b)
+
+# exponentiation
+x = 5
+y = 3
+print(x ** y) #eta kaaj kore power er moto...
+
+# floor division
+num1 = 15
+num2 = 2
+print(num1 // num2)
+
+
+# assignment operators
+m = 5
+res = m + 10
+print(res)
