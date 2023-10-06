2d997bd5dc3ea8f29d007a40e084addd108fc52c
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-06 21:33:39 +0600

    Add files via upload

---
+++ habluExample/listAppendInsert.py
@@ -0,0 +1,18 @@
+# access list item
+list1 = ["rakib", "sakib", "sojib", "rohim", "korim"]
+print(list1)  # print all list item
+print(list1[2])  # print index 2 value
+
+# change list items using index
+list1[4] = "sohel"
+print(list1)
+
+
+# adding data using append(). append diye data add korle list er sheshe add hobe
+list1.append(20)
+print(list1)
+
+# adding data using insert(). insert diye data add korle amra jekhane iccha index diye data add korte parbo.
+list1.insert(1, "Ruhan")
+list1.insert(1, 37)
+print(list1)
