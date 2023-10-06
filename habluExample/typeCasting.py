4bb7354cdaa3e6b50ea9d86f9ee04114eace5740
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-06 15:18:58 +0600

    Update typeCasting.py

--- habluExample/typeCasting.py
+++ habluExample/typeCasting.py
@@ -1,4 +1,5 @@
-#
+# In programming, type conversion is the process of converting data of one type to another. 
+# For example: converting int data to str.
 
 # string to int
 num = "123542474"
@@ -6,10 +7,9 @@ print(int(num))
 print(type(num))  # string type
 print(type(int(num)))  # int type
 
-
 # int to float
 num2 = 50
 print(num2)
-print(type(num2)) # int type
+print(type(num2))  # int type
 print(float(num2))
-print(type(float(num2))) # float type
+print(type(float(num2)))  # float type
