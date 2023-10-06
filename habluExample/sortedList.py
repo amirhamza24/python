a61255ac8d225b3654556bc0c25d5c35027e3eb1
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-07 03:56:20 +0600

    Add files via upload

---
+++ habluExample/sortedList.py
@@ -0,0 +1,16 @@
+# sort list
+# int type
+number = [4, 8, 3, 20, 2, 5, 1]
+number.sort()  # ascending order
+print(number)
+
+number.sort(reverse = True)  # descending order
+print(number)
+
+# string type
+eng = ["b", "u", "k", "a", "c", "x"]
+eng.sort()  # ascending order
+print(eng)
+
+eng.sort(reverse = True)  # descendig order
+print(eng)
