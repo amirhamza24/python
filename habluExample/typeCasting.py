d94ecb72e6725ae03062b36e57d789c9677b23c4
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-06 15:13:16 +0600

    Add files via upload

---
+++ habluExample/typeCasting.py
@@ -0,0 +1,15 @@
+#
+
+# string to int
+num = "123542474"
+print(int(num))
+print(type(num))  # string type
+print(type(int(num)))  # int type
+
+
+# int to float
+num2 = 50
+print(num2)
+print(type(num2)) # int type
+print(float(num2))
+print(type(float(num2))) # float type
