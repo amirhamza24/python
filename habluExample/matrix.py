d4d011d210c3865e6306f3c7cd604d5cf22aa3eb
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-11 22:56:41 +0600

    Add files via upload

---
+++ habluExample/matrix.py
@@ -0,0 +1,8 @@
+matrixList = [
+    [1, 7, "kobir", 8, 5],
+    [3, 6, 1, 4, 9],
+    10
+]
+
+print(matrixList[1][4])   # output: 9
+# ekhane first index ta (matrixList) er index k bujhabe and 2nd index ta fist index er list er index k bujhabe...
