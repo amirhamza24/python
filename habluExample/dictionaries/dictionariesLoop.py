36abcd41f10737bece0f789496f28562576504ea
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-13 03:54:01 +0600

    Add files via upload

---
+++ habluExample/dictionaries/dictionariesLoop.py
@@ -0,0 +1,27 @@
+studentInfo = {
+    "student1": {
+        "name": "Eshan",
+        "group": "Science",
+        "id": 4,
+        "number": 22325343
+    },
+    "student2": {
+        "name": "Jishan",
+        "group": "Commerce",
+        "id": 7,
+        "number": 624255385
+    },
+    "year": 2023
+}
+
+for x in studentInfo:
+    print(x)  # ekhane dictionaries er parent key gulo dekhabe.. output: student1 student2 year
+
+for a in studentInfo.values():  # ekhane sob parent key er value dekhabe
+    print(a)
+
+for b in studentInfo.keys():
+    print(b)
+
+for c in studentInfo.items():
+    print(c)
