1041972b93234e9bf54e4dda9a898c4aa96f3541
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-13 03:52:32 +0600

    Add files via upload

---
+++ habluExample/dictionaries/dictionaries.py
@@ -0,0 +1,29 @@
+studentInfo = {
+    "student1": {
+        "name": "Eshan",
+        "group": "Science",
+        "id": 4,
+        "number": 22325343
+    },
+    "student2": {
+        "name": "Jishan",
+        "group": "Commerce",
+        "id": 7,
+        "number": 624255385
+    },
+    "year": 2023
+}
+
+ # accessing dictionary items
+
+print(studentInfo["student2"]["group"])
+print(studentInfo["year"])
+
+x = studentInfo.get("student2")
+print(x)
+
+key = studentInfo.keys()
+print(key)
+
+value = studentInfo.values()
+print(value)
