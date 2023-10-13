57a29d3c1e9a7b05a555629358c60b190d07a980
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-14 04:02:44 +0600

    Add files via upload

---
+++ habluExample/after50/zip.py
@@ -0,0 +1,6 @@
+
+list1 = [ "Student1", "Student2", "Student3" ]
+list2 = [ "Rakib", "Sakib", "Sojib" ]
+
+print(list(zip(list1, list2)))
+# output:  [('Student1', 'Rakib'), ('Student2', 'Sakib'), ('Student3', 'Sojib')]
