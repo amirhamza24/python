6314fde649f40a47bc2da9bea58efbbb8e0240d8
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-14 04:00:28 +0600

    Add files via upload

---
+++ habluExample/after50/function.py
@@ -0,0 +1,29 @@
+def adding(a, b):
+    sum = a + b
+    print("Sum is: ", sum)
+
+def subtraction(c, d):
+    sub = c - d
+    print("Subtraction is: ", sub)
+
+def multiple(m, n):
+    multi = m * n
+    print("Multiplication is: ", multi)
+
+def division ( x, y):
+    divi = x / y
+    print("Division is: ", divi)
+
+
+adding(10, 20)
+adding(20, 30)
+
+subtraction(20, 10)
+subtraction(50, 30)
+
+multiple(4, 6)
+multiple(6, 3)
+
+division(20, 10)
+division(15, 5)
+
