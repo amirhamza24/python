bb6bd084a2da510d92c9d1548eb629ec7d4a3b7d
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-14 18:05:40 +0600

    Add files via upload

---
+++ habluExample/start65/mathfunctions.py
@@ -0,0 +1,15 @@
+# min & max
+a = min(10, 4, 32, 12, 87, 2, 54)
+b = max(10, 4, 32, 12, 87, 2, 54)
+print(a)
+print(b)
+
+
+# abs().. eta positive number k negative kore and negative k positive kore.
+c = abs(-120)
+print(c)
+
+
+# pow().. eta diye power er kaaj kora hoy
+d = pow(5, 2)  # 5*5
+print(d)
