2e3dc303727b22c9966ee29dfba7207b16f96ef0
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-15 02:03:55 +0600

    Update method.py

--- habluExample/start65/method.py
+++ habluExample/start65/method.py
@@ -1,1 +1,19 @@
+class className:
+    def instanceMethod(self):
+        print("This is instance method")
+
+    @classmethod
+    def classMethod(cls):
+        print("This is class method")
+
+    @staticmethod
+    def staticMethod():
+        print("This is static method")
+
+instance = className()
+
+instance.instanceMethod()
+instance.classMethod()
+instance.staticMethod()
+
 
