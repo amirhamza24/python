80d42458256a190e9971d9ba916856b63b14a75d
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-10-15 10:36:03 +0600

    Add files via upload

---
+++ habluExample/hablupro/madLibsGame.py
@@ -0,0 +1,7 @@
+username = input("What is your name: ")
+fathername = input("What is father name: ")
+mothername = input("What is your mother name: ")
+study = input("Which class do you read in: ")
+relationship = input("Tell me about your relationship status: ")
+
+print(f"My Doggy name is: {username}, My Doggys Father name is: {fathername}, My Doggys Mother name is: {mothername}, My Doggy study in class: {study}, My relationship status is: {relationship}")
